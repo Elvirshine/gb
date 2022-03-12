9b4d3e85f81573bdc2724180de6ebc498eb2db60
Elvirshine <98941841+Elvirshine@users.noreply.github.com>
2022-03-12 16:13:38 +0300

    исправила очередность колонок

--- 11_hw/trigger_products_to_logs.sql
+++ 11_hw/trigger_products_to_logs.sql
@@ -1,4 +1,4 @@
 CREATE DEFINER=`root`@`localhost` TRIGGER `products_AFTER_INSERT` AFTER INSERT ON `products` FOR EACH ROW BEGIN
 	insert into shop.logs (tables_id, tables_name, names, created_at)
-	values (new.id, new.name, 'products', now());
+	values (new.id, 'products', new.name, now());
 END
--- 11_hw/trigger_users_to_logs.sql
+++ 11_hw/trigger_users_to_logs.sql
@@ -1,4 +1,4 @@
 CREATE DEFINER=`root`@`localhost` TRIGGER `users_AFTER_INSERT` AFTER INSERT ON `users` FOR EACH ROW BEGIN
 	insert into shop.logs (tables_id, tables_name, names, created_at)
-	values (new.id, new.name, 'users', now());
+	values (new.id, 'users', new.name, now());
 END
