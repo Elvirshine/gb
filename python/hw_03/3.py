d8b2f95198d4480c79b4a67b4378503d11fbbccb
Elvirshine <98941841+Elvirshine@users.noreply.github.com>
2022-05-15 21:25:59 +0300

    hw 3

--- python/hw_03/3.py
+++ python/hw_03/3.py
@@ -5,9 +5,8 @@ n = list(map(int, input('Введите три целых числа через 
 
 def my_func(c_1, c_2, c_3):
     c = [c_1, c_2, c_3]
-    c.reverse()
-    print(c[0] + c[1])
+    c.remove(min(c))
+    return (sum(c))
 
 
-my_func(n[0], n[1], n[2])
-
+print(my_func(n[0], n[1], n[2]))
