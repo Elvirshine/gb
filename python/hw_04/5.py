00f1ea4853010da05cfaf0760d6ef7269b75b93d
Elvirshine <98941841+Elvirshine@users.noreply.github.com>
2022-05-22 20:53:22 +0300

    hw 4

--- python/hw_04/5.py
+++ python/hw_04/5.py
@@ -3,6 +3,6 @@ print("Задание №5")
 from functools import reduce
 
 start = [el for el in range(100, 1001) if el % 2 == 0]
-print(reduce(lambda x, y: x * y, start))
+print(reduce(lambda x, y: x * y, start, 1))
 print(start)
 
